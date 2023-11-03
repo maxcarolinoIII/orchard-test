c2478b143279ba1e9678cf593abaf2c6c50b20cd
Maximino C. Carolino III <macmaccarolino@gmail.com>
2023-11-03 11:59:37 +0800

    feat: initial setup for first section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex
+      w={"70%"}
+      mx={"auto"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      h={"100vh"}
+      gap={24}
+    >
+      <Image
+        fit={"cover"}
+        h={"604px"}
+        src="/assets/body-needs/Image-01.jpg"
+        alt="Source organic foods"
+      />
+      <Flex flexDir={"column"} gap={24} maxH={"604px"}>
+        <Image fit={"cover"} src="/assets/body-needs/Image-02.jpg" />
+        <Image fit={"cover"} src="/assets/body-needs/Image-03.jpg" />
+      </Flex>
+      <Box>
+        <Heading
+          as={"h1"}
+          color={"white"}
+          textTransform={"uppercase"}
+          fontWeight={"300"}
+          size={"sm"}
+          borderBottom={"1px solid white"}
+          paddingBottom={"14px"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Answer your body's needs
+        </Heading>
+        <Text color={"white"} size={"lg"} fontWeight={"300"}>
+          The way ingredients are sourced affects the way we nourish our bodies.
+          Author Mark Schatzer believes our body naturally develops an appetite
+          for the foods and nutrients it needs to be healthy, but that
+          artificial flavourings are getting in the way. This can be reversed by
+          focusing on high-quality ingredients and being mindful as your
+          appetite guides you to consume according to your body's needs.
+        </Text>
+      </Box>
+    </Flex>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({
+  config,
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: mode("#FFFFFF", "#0E1514")(props),
+      },
+    }),
+  },
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
